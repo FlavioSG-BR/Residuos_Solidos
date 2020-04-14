2513dfed429abf887ce935a9a70889d4a1381f6b
FlavioSG-BR <flavio_godoy@yahoo.com.br>
2020-04-14 15:10:12 -0300

    Cirado tela de listage de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    dados = {'evento':evento}
+    return render(request, 'Informacoes.html', dados)
